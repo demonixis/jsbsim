20f918d4fce3be1275149c46ae969f2d5b8d4fae
Yannick <yannick.comte@novelab.net>
2023-10-25 10:11:25 +0200

    Added a script to build on Android

---
+++ JSBSimForAndroid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf build
+mkdir -p build
+cd build
+
+cmake .. \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+    -DCMAKE_TOOLCHAIN_FILE=/Users/yann/Library/Android/sdk/ndk/25.1.8937393/build/cmake/android.toolchain.cmake \
+    -DANDROID_ABI=arm64-v8a \
+    -DANDROID_PLATFORM=android-29
+
+make -j8
